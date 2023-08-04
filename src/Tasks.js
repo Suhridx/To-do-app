4281578279d3c25ab5fca515fa18fac097311af9
Suhridx <suhrid38@gmail.com>
2023-08-04 17:32:42 +0530

    added all sorts

--- src/Tasks.js
+++ src/Tasks.js
@@ -10,13 +10,34 @@ const Tasks = () => {
   else if (sortBy === "completed")
     state.sort((a, b) => {
       if (a.complete === b.complete) {
-        return 0;
+        return 0
       } else if (a.complete) {
-        return -1;
+        return -1
       } else {
-        return 1;
+        return 1
       }
     })
+  else if(sortBy==="incomplete")
+    state.sort((a, b) => {
+      if (a.complete === b.complete) {
+        return 0
+      } else if (a.complete) {
+        return 1
+      } else {
+        return -1
+      }
+    })
+    else if(sortBy==="important")
+    state.sort((a, b) => {
+      if (a.imp === b.imp) {
+        return 0
+      } else if (a.imp) {
+        return -1
+      } else {
+        return 1
+      }
+    })
+  
   return (
     <div className='container'>
       <p>All Tasks ({state.length} tasks)
@@ -24,6 +45,7 @@ const Tasks = () => {
         <select id="sort-by" onChange={(e) => setSortBy(e.target.value)}>
           <option value="completed">Completed First</option>
           <option value="important">Important First</option>
+          <option value="incomplete">Incomplete First</option>
           <option value="date">Date</option>
         </select>
       </p>
