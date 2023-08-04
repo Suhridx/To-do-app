a5070c24f56c89cd960c0bde946e8191545ed8f3
Suhridx <suhrid38@gmail.com>
2023-08-04 17:22:23 +0530

    added sort by complete

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,17 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import SingleTask from './singletask/SingleTask'
 import { Todo } from './context/TodoContext'
 
 const Tasks = () => {
   const { state } = useContext(Todo)
+  const [sortBy, setSortBy] = useState()
+  if (sortBy === "date")
+    state.sort((a, b) => new Date(a.date) - new Date(b.date));
+  else if (sortBy === "completed")
+    state.sort((a, b) => {
+      if (a.complete === b.complete) {
+        return 0;
+      } else if (a.complete) {
+        return -1;
+      } else {
+        return 1;
+      }
+    })
   return (
     <div className='container'>
       <p>All Tasks ({state.length} tasks)
-      <label htmlFor='sort-by'>Sort By</label>
-        <select id="sort-by">
-          <option value="volvo">Completed</option>
-          <option value="saab">Important</option>
-          <option value="fiat">Date</option>
+        <label htmlFor='sort-by'>Sort By</label>
+        <select id="sort-by" onChange={(e) => setSortBy(e.target.value)}>
+          <option value="completed">Completed First</option>
+          <option value="important">Important First</option>
+          <option value="date">Date</option>
         </select>
       </p>
       {state.map((item, index) => {
